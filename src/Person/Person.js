02f2af2ec9bef1877c2f99c0a9b8e1a490a43f3b
Kamran Ali <kamranalinitb@gmail.com>
2019-06-13 15:18:41 +0530

    Styling the components: Global Style

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Person.css';
 
 /* person is a stateless / Dumb / Presentational component as it has NO internal state management */
 const person = (props) => {
   const {name, age, children, click, changed} = props;
   return (
-    <div>
+    <div className="Person">
       <p onClick={click} > I am {name} and I am {age} years old </p>
       <p> {children} </p>
       <input onChange={changed} defaultValue={name} />
